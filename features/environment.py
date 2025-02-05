9e0fae72d28882e9313b333feef2eb15b2edc527
Ksenija <ksenija.tajur@gmail.com>
2025-02-05 21:11:09 +0200

    driver quit

--- features/environment.py
+++ features/environment.py
@@ -1,9 +1,21 @@
 from selenium import webdriver
 
 def before_scenario(context, scenario):
+    options = webdriver.ChromeOptions()
+    #options.add_argument('--headless')
+    #options.add_argument('--disable-gpu')
+    #options.add_argument('--window-size=1920,1080')
+    
+    #context.driver = webdriver.Chrome(options=options)
     context.driver = webdriver.Chrome()
-    context.driver.maximize_window
+    context.driver.maximize_window()
     
 
-
+def after_scenario(context, scenario):
+    try:
+        if context.driver:
+            context.driver.quit()
+            print("WebDriver quit successfully")
+    except Exception as e:
+        print(f"Error quitting WebDriver: {e}")
     
