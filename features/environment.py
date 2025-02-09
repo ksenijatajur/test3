debfbe71af63d1eaa8c5f01db32eccb00675dd49
Ksenija <ksenija.tajur@gmail.com>
2025-02-09 14:11:37 +0200

    env

--- features/environment.py
+++ features/environment.py
@@ -1,21 +1,24 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
 
 def before_scenario(context, scenario):
-    options = webdriver.ChromeOptions()
-    #options.add_argument('--headless')
-    #options.add_argument('--disable-gpu')
-    #options.add_argument('--window-size=1920,1080')
+    options = Options()
+
+    #
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    # headless mode
+    #options.add_argument("--disable-gpu")  
+    #options.add_argument("--headless=new")  
+
+    # Set up WebDriver
+    service = Service(ChromeDriverManager().install())
+    context.driver = webdriver.Chrome(service=service, options=options)
     
-    #context.driver = webdriver.Chrome(options=options)
-    context.driver = webdriver.Chrome()
     context.driver.maximize_window()
-    
 
 def after_scenario(context, scenario):
-    try:
-        if context.driver:
-            context.driver.quit()
-            print("WebDriver quit successfully")
-    except Exception as e:
-        print(f"Error quitting WebDriver: {e}")
-    
+    if hasattr(context, "driver"):  
+        context.driver.quit()
